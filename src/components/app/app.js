35646fd8e56976f087c77511a4b2149684572e66
NazarFernos <nazarum98@gmail.com>
2019-09-03 21:38:59 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/components/app/app.js
    #	src/components/table/table.js
    #	src/components/todo-list-item/todo-list-item.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,98 +1,133 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import TodoList from '../todo-list';
-import ItemAddForm from '../item-add-form';
-
+import Table from '../table';
+import TableAddRow from '../table-add-row';
+import { logIn } from '../../api/auth';
 
 import './app.css';
 
 export default class App extends Component {
 
-    maxId = 100;
+    maxId = 1;
 
     state = {
-        todoData: [
-            {
-                id: 0, 
-                label: "",
-                price: "",
-                description: ""
-            }
-        ],
-        term: ""
+        tableData: []
     };
 
-    createTodoItem(label, price, description) {
-        let todoData = this.state.todoData;
-        let lastId = todoData[todoData.length-1].id;
-        if (lastId < this.maxId)
-            return {
-                label,
-                price,
-                description,
-                id: ++lastId
-            }
-
-        alert("Product limit exceeded");
-        return false;
-    }
+    createTableItem = (name, price, description) => {
+        return {
+            name,
+            price,
+            description,
+            id: this.maxId++
+        }
+    };
 
     deleteItem = (id) => {
-        this.setState(({ todoData }) => {
+        this.setState(({tableData}) => {
 
-            const idx = todoData.findIndex((el) => el.id === id);
+            const idx = tableData.findIndex((el) => el.id === id);
 
             const newArray = [
-                ...todoData.slice(0, idx),
-                ...todoData.slice(idx + 1)
+                ...tableData.slice(0, idx),
+                ...tableData.slice(idx + 1)
             ];
             return {
-                todoData: newArray
+                tableData: newArray
             };
         });
     };
 
-    addItem = (text, price, description) => {
-        const newItem = this.createTodoItem(text, price, description);
+    addItem = (name, price, description) => {
+        const newItem = this.createTableItem(name, price, description);
 
-        if (!newItem) return
-        
-        this.setState(({ todoData }) => {
+        if (!newItem) return;
+
+        this.setState(({tableData}) => {
 
             const newArr = [
-                ...todoData,
+                ...tableData,
                 newItem
             ];
 
             return {
-                todoData: newArr
+                tableData: newArr
             };
         });
     };
 
-    updateItem(targetId, inputValues) {
-        this.setState(({todoData}) => {
-            const targetIndex = todoData.findIndex((element) => element.id === targetId);
-            const newTodoData = [...todoData];
-            Object.assign(newTodoData[targetIndex], inputValues);
-            return newTodoData;
+    updateItem = (targetId, inputValues) => {
+        this.setState(({tableData}) => {
+            const targetIndex = tableData.findIndex((element) => element.id === targetId);
+            const newTableData = [...tableData];
+            Object.assign(newTableData[targetIndex], inputValues);
+            return newTableData;
         });
+    };
+
+    componentDidMount() {
+        let user = {
+            email: "user1@email.com",
+            password: "!password!"
+        };
+
+        const URL = 'https://gentle-escarpment-19443.herokuapp.com';
+
+        logIn(user)
+            .then(() => getArticlesList());
+
+        const getArticlesList = () =>
+            fetch(`${URL}/v1/articles?page=1&updated_after=1410403761`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': localStorage.getItem('accessToken')
+                }
+            })
+                .then((res) => res.json())
+                .then(
+                    tableData => { this.setState({tableData});
+                        console.log(tableData)
+                    }
+                )
+                .catch((error) => console.log(error));
+
+
+        const deleteArticle = () => {
+            fetch(`${URL}/v1/articles/1`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': localStorage.getItem('accessToken'),
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(user)
+            })
+                .catch((error) => console.log('my err 2', error))
+                .then((res) => {console.log(res); return res.json()})
+                .then(
+                    tableData => {
+                        this.setState({tableData: [tableData]});
+                        console.log(tableData)
+                    }
+                )
+                .catch((error) => console.log('my err', error));
+        }
+
     }
+
 
     render() {
 
-        const { todoData, term } = this.state;
-
+        const {tableData} = this.state;
 
         return (
-            <div className="todo-app">
-                <TodoList
-                    onDeleted = { this.deleteItem }
-                    updateItem = { this.updateItem.bind(this) }
-                    todos={todoData}/>
+            <div className="table-app">
 
+                <TableAddRow addItem={this.addItem}/>
 
-                <ItemAddForm addItem = {this.addItem} />
+                <Table
+                    onDeleted={this.deleteItem}
+                    updateItem={this.updateItem.bind(this)}
+                    tableItems={tableData}/>
             </div>
         );
     }
